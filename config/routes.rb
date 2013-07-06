d34d06dfa3e307717d4f1776fe72e3ee6fd27fd9
irukimo <wangirukimo@gmail.com>
2013-07-06 15:35:11 -0700

    fff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Myapp::Application.routes.draw do
+
+  #root to: 'static_page#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
